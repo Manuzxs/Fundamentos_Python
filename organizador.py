c434121569dd8a0902f0387dbe40863678eef286
Manuzxs <md7613634@gmail.com>
2023-07-17 18:52:30 -0300

    Quinta Aula: Criamos uma função para a manipulação de arquivos

--- organizador.py
+++ organizador.py
@@ -3,6 +3,10 @@ import os
 def option(function):
     return function
 
+def edit(name, action):
+    with open(name, action, encoding="UTF-8") as file:
+            file.write("\nBem vindo a seu arquivo, nele você pode escrever tudo o que você quiser!")
+
 user = input("Qual é o seu nome? ")
 print(f"Hello, {user}! \nEscolha um número no menu e depois digite aqui.")
 while True:
@@ -10,13 +14,15 @@ while True:
     name = input("Tipe the Name: ")
     if menu == "1":
         option(os.mkdir(name))
+    
     elif menu == "2":
         option(os.rmdir(name))
+    
     elif menu == "3":
-        with open(name, "w", encoding="UTF-8") as file:
-            file.write("Bem vindo a seu arquivo, nele você pode escrever tudo o que você quiser!")
+        edit(name, "w")
+    
     elif menu == "4":
         option(os.rm(name))
+    
     elif menu == "5":
-        with open(name, "a", encoding="UTF-8") as file:
-            file.write("\nBem vindo a seu arquivo, nele você pode escrever tudo o que você quiser!")
+        edit(name, "a")
